e35c9ff27e5cddb1a6fda18f300e932196b76fa7
Patrick Crosby <patrick@xblabs.com>
2013-01-04 11:36:42 -0600

    using post instead of get, added test

--- amzses.go
+++ amzses.go
@@ -19,6 +19,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -43,7 +44,7 @@ func SendMail(from, to, subject, body string) (string, error) {
 	data.Add("Message.Body.Text.Data", body)
 	data.Add("AWSAccessKeyId", accessKey)
 
-	return sesGet(data)
+	return sesPost(data)
 }
 
 func SendMailHTML(from, to, subject, bodyText, bodyHTML string) (string, error) {
@@ -56,7 +57,7 @@ func SendMailHTML(from, to, subject, bodyText, bodyHTML string) (string, error) 
 	data.Add("Message.Body.Html.Data", bodyHTML)
 	data.Add("AWSAccessKeyId", accessKey)
 
-	return sesGet(data)
+	return sesPost(data)
 }
 
 func authorizationHeader(date string) []string {
@@ -106,6 +107,44 @@ func sesGet(data url.Values) (string, error) {
 
 		log.Printf("error response: %s", resultbody)
 		return "", errors.New(string(resultbody))
+	}
+
+	return string(resultbody), nil
+}
+
+func sesPost(data url.Values) (string, error) {
+        body := strings.NewReader(data.Encode())
+        req, err := http.NewRequest("POST", endpoint, body)
+        if err != nil {
+                return "", err
+        }
+        req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	now := time.Now().UTC()
+	// date format: "Tue, 25 May 2010 21:20:27 +0000"
+	date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+        req.Header.Set("Date", date)
+
+	h := hmac.New(sha256.New, []uint8(secretKey))
+	h.Write([]uint8(date))
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", accessKey, signature)
+	req.Header.Set("X-Amzn-Authorization", auth)
+
+	r, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("http error: %s", err)
+		return "", err
+	}
+
+	resultbody, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
+	if r.StatusCode != 200 {
+		log.Printf("error, status = %d", r.StatusCode)
+
+		log.Printf("error response: %s", resultbody)
+                return "", errors.New(fmt.Sprintf("error code %d. response: %s", r.StatusCode, resultbody))
 	}
 
 	return string(resultbody), nil
---
+++ send_test.go
@@ -0,0 +1,47 @@
+package amzses
+
+import (
+        "flag"
+        "testing"
+)
+
+var to, from string
+
+func init() {
+        flag.StringVar(&to, "to", "", "email recipient")
+        flag.StringVar(&from, "from", "", "email sender")
+}
+
+func checkFlags(t *testing.T) {
+        if len(to) == 0 {
+                t.Fatal("must specify recipient via -to flag.")
+        }
+        if len(from) == 0 {
+                t.Fatal("must specify sender via -from flag.")
+        }
+}
+
+func TestText(t *testing.T) {
+        checkFlags(t)
+        _, err := SendMail(from, to, "amzses text test", textBody)
+        if err != nil {
+                t.Fatal(err)
+        }
+}
+
+func TestHtml(t *testing.T) {
+        checkFlags(t)
+        _, err := SendMailHTML(from, to, "amzses html test", textBody, htmlBody)
+        if err != nil {
+                t.Fatal(err)
+        }
+}
+
+var textBody = `This is an example email body for the amzses go package.`
+
+var htmlBody = `
+This is an <b>html email</b>.
+<br/>
+<br/>
+<img src="http://placehold.it/600x200/">
+`
